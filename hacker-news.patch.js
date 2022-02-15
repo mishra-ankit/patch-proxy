3c86ada72ef1f7cc9089d02ec24f5927ee11899e
Ankit Mishra <1507256+mishra-ankit@users.noreply.github.com>
2022-02-15 08:41:03 +0100

    init

---
+++ hacker-news.patch.js
@@ -0,0 +1,62 @@
+function apply() {
+    const storageKeyPrefix = 'hacker-news';
+    document.querySelectorAll('.athing').forEach((elem) => {
+        const button = htmlToElement(`<button>+</button>`);
+        elem.append(button);
+        const link = elem.querySelector('a.titlelink');
+        button.onclick = () => {
+            console.log(link.href);
+            console.log(link.innerText);
+            const now = Date.now()
+            const data = {
+                href: link.href,
+                title: link.innerText,
+                date: now
+            }
+            localStorage.setItem(`${storageKeyPrefix}-${now}`, JSON.stringify(data));
+        }
+    }
+    )
+
+    const bookmarkButton = htmlToElement('<button>Saved</button>');
+    document.querySelector('.pagetop').appendChild(bookmarkButton)
+    const itemlist = document.querySelector('.itemlist');
+    bookmarkButton.onclick = () => {
+        itemlist.innerText = ''
+
+        itemsHTML = getAll().map((({ href, title }, index) => {
+            const itemTemplate = `<tr class="athing">
+            <td align="right" valign="top" class="title"><span class="rank">${index + 1}.</span></td>
+            <td></td>
+            <td class="title">
+                <a
+                href="${href}"
+                class="titlelink">${title}</a>
+            </td>
+            </tr>
+            <tr>
+                <td class="subtext"><br/></td>
+            </tr>
+        `
+            return itemTemplate;
+        }));
+
+        const finalHTML = `<tbody>${itemsHTML}</tbody>`;
+        itemlist.append(htmlToElement(finalHTML));
+    }
+
+    function htmlToElement(html) {
+        var template = document.createElement("template");
+        html = html.trim();
+        // Never return a text node of whitespace as the result
+        template.innerHTML = html;
+        return template.content.firstChild;
+    }
+
+    function getAll() {
+        return Object.entries(localStorage)
+        .filter(([key]) => (key.startsWith(storageKeyPrefix)))
+        .map(i => JSON.parse(i[1]))
+        .sort((i, j) => (j.date - i.date));
+    }
+}
